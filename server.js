7abcd617aa8466641463d1c9b5fbb90e6b5a3a30
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-10 12:11:47 +0800

    implements the payload

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ let express = require('express');
 
 let app = express();
 
-app.get('/api/whoami', (res, req) => {
+app.get('/api/whoami', (req, res) => {
+  let payload = {
+    ipaddress: null,
+    language: null,
+    software: null,
+  };
 
+  payload.language = req.get('Accept-Language').split(',')[0];
+  payload.ipaddress = req.ip;
+  payload.software= req.get('User-Agent').split(/\(([^\(]+)\)/)[1];
+
+  res.json(payload);
 });
 
 app.listen(process.env.PORT || 3000);
